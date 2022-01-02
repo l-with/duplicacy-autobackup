f78c4d8f1a2210dadff48d9417d68ba625db020d
Lars With <lars@with.de>
2022-01-02 11:27:32 +0100

    make schedule optional

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-echo "$BACKUP_SCHEDULE /app/duplicacy-autobackup.sh backup" > /var/spool/cron/crontabs/root
-echo "$PRUNE_SCHEDULE /app/duplicacy-autobackup.sh prune" >> /var/spool/cron/crontabs/root
+if [ -n "${BACKUP_SCHEDULE+1}" ]; then
+    echo "$BACKUP_SCHEDULE /app/duplicacy-autobackup.sh backup" > /var/spool/cron/crontabs/root
+fi
+if [ -n "${PRUNE_SCHEDULE+1}" ]; then
+    echo "$PRUNE_SCHEDULE /app/duplicacy-autobackup.sh prune" >> /var/spool/cron/crontabs/root
+fi
 
 cd /wd
 
@@ -13,4 +17,6 @@ if [[ $BACKUP_IMMEDIATLY == "yes" ]] || [[ $BACKUP_IMMEDIATELY == "yes" ]]; then
     /app/dispose-symlinks.sh
 fi
 
-crond -l 8 -f
+if [ -n "${BACKUP_SCHEDULE+1}" ] || [ -n "${PRUNE_SCHEDULE+1}" ]; then
+    crond -l 8 -f
+fi
